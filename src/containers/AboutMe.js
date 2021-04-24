058fcb622829670182d0ec419e11db79fd647a15
Stud Horse <lalalatim@yahoo.com.tw>
2021-04-24 23:50:58 +0800

    fixed pdf

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -6,8 +6,8 @@ const AboutMe = () => {
         <div>
             <h1 className="tc f2-l f3 mt0-l mb5-l mt4 mb2"><span className="f1-l f2 fw1">| </span>02 : About Me<span className="f1-l f2 fw1"> |</span></h1>
             <div className="h-100" style={{ lineHeight: "2em" }}>
-                <p className="ml7-l ml5 pt4 w-60">Being a Self-learning web developer, the motives pushing me through is the thrills I get from technical breakthroughs. Cultural gaps should not be obstacles but appreciatable features.</p>
-                <div className="ml7-l ml5 w-50">
+                <p className="ml7-l ml5 pt4 w-70">Being a Self-learning web developer, the motives pushing me through is the thrills I get from technical breakthroughs. Cultural gaps should not be obstacles but appreciatable features.</p>
+                <div className="ml7-l ml5 w-70">
                     <h2>Skills</h2>
                     <ul>
                         <li>React.js</li>
